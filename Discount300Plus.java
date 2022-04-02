3515d896fa98a8ace52599282f04cbd7cfd3173e
s24588-pj <92211768+s24588-pj@users.noreply.github.com>
2022-04-02 15:42:10 +0200

    Update Discount300Plus.java

--- Discount300Plus.java
+++ Discount300Plus.java
@@ -1,17 +1,20 @@
-
 package pl.pjait;
 
-public class Discount300Plus implements ICanCalculateSpecialOffer{
+public class Discount300Plus implements ICanCalculateSpecialOffer
+{
 
     @Override
-    public boolean canCalculate(Cart cart) {
+    public boolean canCalculate(Cart cart) 
+    {
         CartService cartService = new CartService();
         return cartService.getSumOfCart(cart) > 300;
     }
 
     @Override
-    public void calculateOffer(Cart cart) {
-        for(Product product: cart.getProducts()){
+    public void calculateOffer(Cart cart)
+    {
+        for(Product product: cart.getProducts())
+        {
             product.setDiscountPrice(0.95 * product.getPrice());
         }
     }
