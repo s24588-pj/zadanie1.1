61d54a622aeee24c9c9a51595e7138f434a7d397
s24588-pj <92211768+s24588-pj@users.noreply.github.com>
2022-04-02 15:41:01 +0200

    Update ApplyFreeCup.java

--- ApplyFreeCup.java
+++ ApplyFreeCup.java
@@ -1,15 +1,18 @@
 package pl.pjait;
 
-public class ApplyFreeCup implements ICanCalculateSpecialOffer{
+public class ApplyFreeCup implements ICanCalculateSpecialOffer
+{
 
     @Override
-    public boolean canCalculate(Cart cart) {
+    public boolean canCalculate(Cart cart) 
+    {
         CartService cartService = new CartService();
         return cartService.getSumOfCart(cart) > 200;
     }
 
     @Override
-    public void calculateOffer(Cart cart) {
+    public void calculateOffer(Cart cart) 
+    {
         Product cup = new Product("999", "kubek", 0, 0);
         cart.addNewProductToCart(cup);
     }
