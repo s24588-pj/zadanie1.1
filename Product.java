e64147a075fdab38381df31896cfc3bcfaf3ee63
s24588-pj <92211768+s24588-pj@users.noreply.github.com>
2022-04-02 15:43:23 +0200

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,56 +1,68 @@
 
 package pl.pjait;
 
-public class Product {
+public class Product 
+{
     private String code;
     private String name;
     private double price;
     private double discountPrice;
 
-    public Product(String code, String name, double price, double discountPrice) {
+    public Product(String code, String name, double price, double discountPrice) 
+    {
         this.code = code;
         this.name = name;
         this.price = price;
         this.discountPrice = discountPrice;
     }
 
-    public Product() {
+    public Product() 
+    {
     }
 
-    public String getCode() {
+    public String getCode() 
+    {
         return code;
     }
 
-    public void setCode(String code) {
+    public void setCode(String code) 
+    {
         this.code = code;
     }
 
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         this.name = name;
     }
 
-    public double getPrice() {
+    public double getPrice()
+    {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(double price) 
+    {
         this.price = price;
     }
 
-    public double getDiscountPrice() {
+    public double getDiscountPrice() 
+    {
         return discountPrice;
     }
 
-    public void setDiscountPrice(double discountPrice) {
+    public void setDiscountPrice(double discountPrice) 
+    {
         this.discountPrice = discountPrice;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "\nProduct{" +
                 "code='" + code + '\'' +
                 ", name='" + name + '\'' +
