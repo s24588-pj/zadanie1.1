d48add348b8629deb7c863e45d498025fdea699a
s24588-pj <92211768+s24588-pj@users.noreply.github.com>
2022-04-02 15:40:23 +0200

    Update CartService.java

--- CartService.java
+++ CartService.java
@@ -5,43 +5,52 @@ import java.util.List;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 
-public class CartService {
+public class CartService 
+{
 
-    public Product getCheapestOne(List<Product> products) {
+    public Product getCheapestOne(List<Product> products) 
+    {
         Product temporaryProduct = new Product();
         temporaryProduct.setPrice(999999);
-        for (Product product : products) {
-            if (product.getPrice() <= temporaryProduct.getPrice()) {
+        for (Product product : products) 
+        {
+            if (product.getPrice() <= temporaryProduct.getPrice()) 
+            {
                 temporaryProduct = product;
             }
         }
         return temporaryProduct;
     }
 
-    public Product getExpensiveOne(List<Product> products) {
+    public Product getExpensiveOne(List<Product> products) 
+    {
         Product temporaryProduct = new Product();
         temporaryProduct.setPrice(0);
-        for (Product product : products) {
-            if (product.getPrice() >= temporaryProduct.getPrice()) {
+        for (Product product : products) 
+        {
+            if (product.getPrice() >= temporaryProduct.getPrice()) 
+            {
                 temporaryProduct = product;
             }
         }
         return temporaryProduct;
     }
 
-    public List<Product> sortProductsByName(List<Product> products) {
-        return products.stream().sorted(Comparator.comparing(Product::getName))
-                .collect(Collectors.toList());
+    public List<Product> sortProductsByName(List<Product> products) 
+    {
+        return products.stream().sorted(Comparator.comparing(Product::getName)).collect(Collectors.toList());
     }
 
-    public List<Product> sortProductsByPrice(List<Product> products) {
-        return products.stream().sorted(Comparator.comparing(Product::getPrice))
-                .collect(Collectors.toList());
+    public List<Product> sortProductsByPrice(List<Product> products) 
+    {
+        return products.stream().sorted(Comparator.comparing(Product::getPrice)).collect(Collectors.toList());
     }
 
-    public double getSumOfCart(Cart cart) {
+    public double getSumOfCart(Cart cart) 
+    {
         double totalPrice = 0;
-        for (Product product : cart.getProducts()) {
+        for (Product product : cart.getProducts()) 
+        {
             totalPrice += product.getPrice();
         }
         return totalPrice;
